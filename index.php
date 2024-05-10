754d9e9adf5711168fb5bdf3271ad457024ae44a
sisiliadwi <127425515+sisiliadwi@users.noreply.github.com>
2024-05-10 21:49:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 include_once 'controller/routes.php';
+include_once 'config/static.php';
+include_once 'controller/main.php';
+include_once 'function/main.php';
+include_once 'config/env.php';
+
+new Router();
