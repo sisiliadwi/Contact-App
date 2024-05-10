a417c5ae49e99e28f35c872ec8b196434561de8b
sisiliadwi <127425515+sisiliadwi@users.noreply.github.com>
2024-05-10 21:46:34 +0700

    Update conn.php

--- config/conn.php
+++ config/conn.php
@@ -1,12 +1,12 @@
 <?php
-require_once 'env.php';
+include_once 'env.php';
 
 $host = $_ENV['DB_HOST'];
 $username = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
 $database = $_ENV['DB_NAME'];
 
-$conn = new mysqli($host, $username, $password, $database);
+$conn = new mysqli('localhost', 'root', '', 'contact');
 if ($conn->connect_error) {
     die("Koneksi database gagal: " . $conn->connect_error);
 }
